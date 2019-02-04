7a79f6210e67d365fdedcbfb2bfe130c530c556b
hitsuji-haneta <hitsuji.haneta1986@gmail.com>
2019-02-04 22:04:50 +0900

    install eosjs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Api, JsonRpc, RpcError } from 'eosjs';
+import JsSignatureProvider from 'eosjs/dist/eosjs-jssig';
+const defaultPrivateKey = "PW5KZfGY9xTQh9HB1meEsD9Ur6a49zLw9d5b3bCokKm1hEqLDr9qu"; // useraaaaaaaa
+const signatureProvider = new JsSignatureProvider([defaultPrivateKey]);
+const rpc = new JsonRpc('http://127.0.0.1:7777');
+const api = new Api({ rpc, signatureProvider });
 
 class App extends Component {
+
+  state = { result: 'result' };
+
+  async componentDidMount() {
+    const result = await api.transact({
+      actions: [{
+        account: 'eosio.token',
+        name: 'transfer',
+        authorization: [{
+            actor: 'alice',
+            permission: 'active',
+        }],
+        data: {
+            from: 'alice',
+            to: 'bob',
+            quantity: '0.0001 SYS',
+            memo: '',
+        },
+    }]
+    }, {
+      blocksBehind: 3,
+      expireSeconds: 30,
+    });
+    this.setState({ result })
+  }
   render() {
     return (
       <div className="App">
@@ -18,6 +49,7 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Learn React
+            {this.state.result}
           </a>
         </header>
       </div>
